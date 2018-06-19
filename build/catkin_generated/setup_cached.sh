7c42986bf653308cc5fee700e5ae381354f9632d
Virtual <reyhaneh.pahlevan@gmail.com>
2018-06-18 20:18:41 -0400

    laser info

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,10 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/virtual/RoboCup2018RVRL_Demo/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/virtual/RoboCup2018RVRL_Demo/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/virtual/RoboCup2018RVRL_Demo/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/virtual/RoboCup2018RVRL_Demo/build"
-export PYTHONPATH="/home/virtual/RoboCup2018RVRL_Demo/devel/lib/python2.7/dist-packages:$PYTHONPATH"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/virtual/RoboCup2018RVRL_Demo/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/virtual/RoboCup2018RVRL_Demo/src:$ROS_PACKAGE_PATH"
--- build/hector_quadrotor_gazebo_plugins/catkin_generated/installspace/hector_quadrotor_gazebo_pluginsConfig.cmake
+++ build/hector_quadrotor_gazebo_plugins/catkin_generated/installspace/hector_quadrotor_gazebo_pluginsConfig.cmake
@@ -129,7 +129,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/virtual/RoboCup2018RVRL_Demo/install/lib;/opt/ros/kinetic/lib)
+    foreach(path /home/virtual/RoboCup2018RVRL_Demo/install/lib;/home/virtual/RoboCup2018RVRL_Demo/devel/lib;/opt/ros/kinetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
